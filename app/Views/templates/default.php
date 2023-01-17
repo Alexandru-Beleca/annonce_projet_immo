5126e0dd38bc666f3705714dd5296e7563764e3b
Alexandru-Beleca <beleca.alexandru.simplon@gmail.com>
2023-01-17 09:44:36 +0100

    first repo

---
+++ app/Views/templates/default.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title><?= App::getInstance()->title; ?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="../asset/css/app.css" > 
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top ">
+    <div class="container">
+        <div class="navbar-header ">
+            
+            <ul class="nav">
+                <li class="nav-item ">
+                    <a class="navbar-brand" href="index.php">AmaHome</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?p=users.login">Admin</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+
+    <div class="starter-template" style="padding-top: 50px;">
+        <?= $content; ?>
+    </div>
+
+</div><!-- /.container -->
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<footer>
+        
+
+</footer>
+</html>
+</html>
---
+++ app/Views/users/login.php
@@ -0,0 +1,11 @@
+<?php if($errors): ?>
+    <div class="alert alert-danger">
+        Identifiants incorrects
+    </div>
+<?php endif; ?>
+
+<form method="post">
+    <?= $form->input('username', 'Pseudo'); ?>
+    <?= $form->input('password', 'Mot de passe', ['type' => 'password']); ?>
+    <button class="mt-2 btn btn-primary">Envoyer</button>
+</form>
